b3c23670b924e179af174d4e790043ad67610e33
onecoders <onecoders@gmail.com>
2014-03-17 15:19:53 +0800

    init grid menu in main activity

--- EasyRecharge/src/my/project/easyrecharge/activity/ActMain.java
+++ EasyRecharge/src/my/project/easyrecharge/activity/ActMain.java
@@ -1,18 +1,57 @@
 package my.project.easyrecharge.activity;
 
 import my.project.easyrecharge.R;
+import my.project.easyrecharge.adapter.AdaMenu;
+import my.project.easyrecharge.model.IndexMenu;
 import my.project.easyrecharge.view.MyDialog.OnOKClickListener;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 
-public class ActMain extends ActBase implements OnClickListener {
+public class ActMain extends ActBase implements OnClickListener,
+		OnItemClickListener {
+
+	private GridView gridMenu;
+	private IndexMenu[] menus;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.act_main);
+		init();
+	}
+
+	private void init() {
+		initMenuList();
+		initGridMenu();
+	}
+
+	private void initMenuList() {
+		String[] menuTitles = getResources().getStringArray(R.array.menu_title);
+		TypedArray iconArray = getResources().obtainTypedArray(
+				R.array.menu_icon_id);
+		menus = new IndexMenu[menuTitles.length];
+		for (int i = 0; i < menus.length; i++) {
+			menus[i] = new IndexMenu(iconArray.getResourceId(i, 0),
+					menuTitles[i]);
+		}
+		iconArray.recycle();
+	}
+
+	private void initGridMenu() {
+		gridMenu = (GridView) findViewById(R.id.grid_menu);
+		gridMenu.setAdapter(new AdaMenu(this, menus));
+		gridMenu.setOnItemClickListener(this);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
 	}
 
 	@Override
--- EasyRecharge/src/my/project/easyrecharge/adapter/AdaMenu.java
+++ EasyRecharge/src/my/project/easyrecharge/adapter/AdaMenu.java
@@ -1,22 +1,34 @@
 package my.project.easyrecharge.adapter;
 
+import my.project.easyrecharge.R;
 import my.project.easyrecharge.model.IndexMenu;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AdaMenu extends ArrayAdapter<IndexMenu> {
 
-	public AdaMenu(Context context, int resource, IndexMenu[] objects) {
-		super(context, resource, objects);
+	public AdaMenu(Context context, IndexMenu[] objects) {
+		super(context, 0, objects);
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+		IndexMenu item = getItem(position);
 		if (convertView == null) {
-
+			convertView = LayoutInflater.from(getContext()).inflate(
+					R.layout.view_item_menu, null);
 		}
+		ImageView menuIcon = (ImageView) convertView
+				.findViewById(R.id.menu_icon);
+		menuIcon.setImageResource(item.getIconId());
+		TextView menuTitle = (TextView) convertView
+				.findViewById(R.id.menu_title);
+		menuTitle.setText(item.getTitle());
 		return convertView;
 	}
 
